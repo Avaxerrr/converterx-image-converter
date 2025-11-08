b0de9a2bea5c9d22fa22bc2dfebbd39916c2d5b5
Avaxerrr <kevieralbarracin@hotmail.com>
2025-11-08 09:42:04 +0800

    Commit 17-B-6 - Update core folder init module

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from .converter import ImageConverter
 from .format_settings import ConversionSettings, ImageFormat
 from .output_preview_generator import OutputPreviewGenerator
+from .app_settings import SettingsKeys, AppSettingsController
 
 __all__ = ['ImageConverter', 'ConversionSettings', 'ImageFormat', 'OutputPreviewGenerator']
